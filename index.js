9921709481404d0f4ac86d2fbc6b96fe33124770
mary_waiyego <waiyegomary55@gmail.com>
2023-03-14 18:41:25 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+const sum = (parameter1, parameter2) => {
+    console.log(`Adding ${parameter1}`);
+    console.log(`Adding ${parameter2}`);
+    return parameter1 + parameter2;
+}
+sum(1, 2); //=> 3
+const divide =  () =>{
+    return 2000/100;
+}
+const square = () => {
+    return 2 * 2;
+}
+const add = () => {
+    return 3 + 4;
+}
